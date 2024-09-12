715712d90a3158ef87c15b85682b60c27c3d4e01
Mubashir <muhammadmubashis6457@gmail.com>
2024-09-12 07:14:16 +0500

    hyfi

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-          “Hello!I am Mubashir.I am AI student.I am very happy to learn about AI.”
+          “Hello I am Mubashir.I am AI student.I am very happy to learn about AI.”
         </p>
       </blockquote>
       <figcaption className="font-medium">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
+import next from 'next'
 import React from 'react'
-
 const page = () => {
   return (
-    <div>homepag
+    <div>homepage
     </div>
   
   )
