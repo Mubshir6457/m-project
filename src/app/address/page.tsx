f546cd5e858c298abd46c09bb02390a8450dd9a6
Mubashir <muhammadmubashis6457@gmail.com>
2024-09-17 15:13:24 +0500

    update

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import homeimage from "@/component/images/h.pic.png"
 const page = () => {
   return (
     <div><figure className="md:flex bg-yellow-200 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/h.pic.png" alt="" width="384" height="512"/>
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={homeimage} alt="" width="384" height="512"/>
     <div className="md:p-text-left md:text-left space-y-4">
       <blockquote>
-        <p className="text-lg font-medium">
+        <p className="text-lg font">
           “My house situated in Baghdad Colony near Wirless Pul in Rahim yar khan.Rahim yar khan is a beautiful city.I like it so much.”
         </p>
       </blockquote>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
+import homeimage from "@/component/images/p-pic.png"
 const page = () => {
   return (
     <div><figure className="md:flex gap-6 p-3 bg-yellow-200 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <Image className="w-24 h-24 md:w-20 md:h-auto md:rounded-none rounded-full mx-auto" src="/p-pic.png" alt="" width="384" height="512"/>
+    <Image className="w-24 h-24 md:w-20 md:h-auto md:rounded-none rounded-full mx-auto" src={homeimage} alt="" width="384" height="512"/>
     <div className="pt-6 md:p-20 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
